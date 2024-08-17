0a4e49233ea477d2701569525f28ca161c750009
Weverton Teixeira <69995549+weversousa@users.noreply.github.com>
2024-08-16 21:54:12 -0300

    build(docker): atualiza post-start-command.bash
    Foram adicionados os códigos que editam os arquivos JSON, assim não é mais preciso realizar essa configuração de forma manual após a instalação das bibliotecas. E depois de editados é executado o Biome para formatá-los.

--- .devcontainer/post-start-command.bash
+++ .devcontainer/post-start-command.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+PACKAGE_JSON="package.json";
+BIOME_JSON="biome.json";
+
+npm init --yes;
+
 npm install --save-dev --save-exact @biomejs/biome && npx @biomejs/biome init;
 
 npm install --save-dev lint-staged;
@@ -13,3 +18,10 @@ echo "#!/bin/sh" > .husky/pre-commit && echo "npx lint-staged" >> .husky/pre-com
 echo "#!/bin/sh" > .husky/commit-msg && echo "npx --no -- commitlint --edit \$1" >> .husky/commit-msg;
 
 npm install --save-dev commitizen && npx commitizen init cz-conventional-changelog --save-dev --save-exact;
+
+jq 'to_entries | ( .[:(index(.[] | select(.key == "main")) + 1)] + [{"key": "type", "value": "module"}] + .[(index(.[] | select(.key == "main")) + 1):] ) | from_entries' $PACKAGE_JSON > temp.json && mv temp.json $PACKAGE_JSON;
+jq 'to_entries | ( .[:(index(.[] | select(.key == "scripts")) + 1)] + [{"key": "lint-staged", "value": {"*.ts": ["npx biome check --write --no-errors-on-unmatched"]}}] + .[(index(.[] | select(.key == "scripts")) + 1):] ) | from_entries' $PACKAGE_JSON > temp.json && mv temp.json $PACKAGE_JSON;
+jq '(.scripts |= . + {"commit": "cz"})' $PACKAGE_JSON > temp.json && mv temp.json $PACKAGE_JSON;
+jq '. + {"formatter": {"enabled": true, "indentStyle": "space", "indentWidth": 2}, "javascript": {"formatter": {"quoteStyle": "single"}}}' "$BIOME_JSON" > temp.json && mv temp.json "$BIOME_JSON";
+
+npx biome check --write --no-errors-on-unmatched;
